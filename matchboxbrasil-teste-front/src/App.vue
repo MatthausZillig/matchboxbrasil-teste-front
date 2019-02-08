7fb42a4f1975939c16e6bac0f7f30956e94677da
MatthausZillig <matthaus.zillig@proatecnologia.com.br>
2019-02-08 13:53:04 -0200

    Ajustes

--- matchboxbrasil-teste-front/src/App.vue
+++ matchboxbrasil-teste-front/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- Container -->
     <div class="container p-5">
       <!--Header-->
       <div class="row justify-content-md-center">
@@ -12,17 +13,20 @@
           </div>
         </header>
       </div>
+      <!-- end header -->
 
       <!-- Ranking List -->
       <div class="row justify-content-md-center">
         <section class="mt-3 col-4">
           <b-list-group class="col-12">
-            <b-list-group-item v-b-popover.hover="item.positive" title="Popover Title" id="popoverButton-sync" class="text-monospace" v-for="item in arrRankingList" v-bind:key="item" button><b-img left rounded="circle" width="75" class="mr-4" :src="item.picture" alt="left image"/>{{item.name}}<p>{{item.description}}</p></b-list-group-item>
+            <b-list-group-item v-b-popover.hover="item.positive" title="Popover Title" id="popoverButton-sync" class="text-monospace" v-for="item in arrRankingList" v-bind:key="item.name" button><b-img left rounded="circle" width="75" class="mr-4" :src="item.picture" alt="left image"/><b-badge variant="primary">{{rankingNumber}}</b-badge>{{item.name}}<p id="p">{{item.description}}</p></b-list-group-item>
           </b-list-group>
         </section>
       </div>
+      <!-- end ranking list -->
 
     </div>
+    <!-- end container -->
   </div>
 </template>
 
@@ -33,16 +37,25 @@ export default {
   name: 'app',
   data () {
     return {
-      arrRankingList: []
-
+      arrRankingList: [],
+      rankingNumber: ''
     }
   },
+  methods: {
+    // rankingNumber:function () {
+    //   let a = 1
+    //     a++
+    //     this.rankingNumber = a
+    // }
+  },
   mounted () {
+    // this.rankingNumber()
+    // GET para consumir o json
     axios
       .get('./src/matchboxbrasil.json')
       .then(response => {
-        this.arrRankingList = response.data.data
-        console.log(this.arrRankingList)
+        this.arrRankingList = response.data.data // encapsular o array de objetos do arquivo json no array criado em data()
+        console.log(response.data.data.name)
       })
       .catch(error => {
         console.log(error)
@@ -54,14 +67,24 @@ export default {
 </script>
 
 <style lang="scss">
+// importação de uma fotne externa
 @import url('https://fonts.googleapis.com/css?family=Montserrat');
+// importação do styles para a SPA
 @import './assets/styles/variables';
 @import './assets/styles/bootstrap';
+
+/*Sobrescrever algumas propriedades do bootstrap vue*/
 #card {
   width: 90% !important;
   margin-left: 15px !important;
 }
-#logo {
-
+/*Id para determinar a font-size da tag p*/
+#p {
+  font-size: 12px !important;
+}
+/*Sobrescrever o componente badge do bootstrap vue*/
+.badge {
+  margin-right: 10px !important;
+  border-radius: 0.55rem;
 }
 </style>
